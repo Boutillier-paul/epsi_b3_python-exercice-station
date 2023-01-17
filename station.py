64315e45caaa5f53b55f19008ab8c9134cdd8dc8
Boutillier Paul <boutillier.paul@gmail.com>
2023-01-17 11:54:35 +0100

    feat: finished

--- station.py
+++ station.py
@@ -6,6 +6,7 @@ from pompe import Pompe
 from dataclasses import dataclass
 
 import json
+import requests
 
 @dataclass
 class Station:
@@ -34,16 +35,53 @@ class Station:
             position = Position(data["geom"][0], data["geom"][1]),
             flag_automate_24 = data["horaires_automate_24_24"],
             pompes = [Pompe(data["prix_valeur"], data["prix_nom"])],
-            services = data["services_service"].split("//")
+            services = data["services_service"].split("//") if "services_service" in data else []
         )
 
     @staticmethod
     def parse_from_text(data: str) -> dict:
         stations_raw_list = json.loads(data)["records"]
+        stations_list = list()
 
-        # Ca c'est pas bon
-        stations_list = list({v['id']:v for v in stations_raw_list}.values())
+        for s_raw in stations_raw_list:
+            station = Station.from_dict(s_raw["fields"])
+            merged = False
 
-        
+            for s in stations_list:
+                if station.id == s.id:
+                    stations_list[stations_list.index(s)].pompes.append(s.pompes[0])
+                    merged = True
+
+            if not merged:
+                stations_list.append(station)
 
         return stations_list
+
+    @staticmethod
+    def filter_by_service(stations: list, searched_service: str) -> list:
+        searched_service_stations_list = list()
+        
+        for s in stations:
+            if searched_service in s.services:
+                searched_service_stations_list.append(s)
+        
+        return searched_service_stations_list
+
+    @staticmethod
+    def sort_by_carburant(stations: list, fuel_type: str) -> list:
+        searched_fuel_stations_list = [s for s in stations for fuel in s.pompes if fuel_type == fuel.libelle]
+        sorted_stations = sorted(searched_fuel_stations_list, key=lambda s: next((fuel.price for fuel in s.pompes if fuel.libelle == fuel_type),float("inf")))
+        return sorted_stations
+
+    
+class StationService:
+    def __init__(self):
+        pass
+
+    def find_station_by_ville(self, ville: str) -> list:
+        url = "https://data.economie.gouv.fr/api/records/1.0/search/?"
+        params = {"dataset": "prix-carburants-fichier-instantane-test-ods-copie", "q": f"ville={ville.upper()}"}
+        request = requests.get(url=url, params=params)
+        data = Station.parse_from_text(request.content)
+        return data
+
